73958ec92ab3fedb3e63650ba6fe5e68b951608c
Vasileios Souvatzis <souvatzisbill@gmail.com>
2017-10-31 02:18:33 +0200

    comit

--- res/js/main.js
+++ res/js/main.js
@@ -1,21 +1,104 @@
 $( document ).ready(function() {
 
     door = $(".door");
-    initializeDoors();
-    door.click(function () {
-        doorCover = $(this).find($(".door-cover"));
-        $(doorCover).fadeOut();
+    doorCover = $(".door-cover");
+    doorContent = $(".door-content");
+    repeat = $(".btn-repeat");
 
+    keepCounter = 0;
+    gamesPlayedKeepCounterObj = $("#gamesPlayedKeep");
+    wonKeepCounter = 0;
+    gamesWonKeepCounterObj = $("#gamesWonKeep");
+    winKeepPercObj = $("#winPercKeep");
+
+    switchCounter = 0;
+    gamesPlayedSwitchCounterObj = $("#gamesPlayedSwitch");
+    wonSwitchCounter = 0;
+    gamesWonSwitchCounterObj = $("#gamesWonSwitch");
+    winSwitchPercObj = $("#winPercSwitch");
+
+    initializeDoors();
+
+    door.click(function () {
+        switch(stage) {
+            case 1:
+                firstChoiceDoor = this;
+                do{
+                    console.log("entered");
+                    revealedDoorIndex = Math.floor(Math.random() * 2);
+                    revealedDoor = wrongDoors[revealedDoorIndex];
+                }while (revealedDoor === this);
+                wrongDoors.splice(revealedDoorIndex, 1);
+                doorCoverObj = $(revealedDoor).find(doorCover);
+                $(doorCoverObj).fadeOut();
+                info = $("#info");
+                info.html("The game master has decided to reveal a door that contains nothing! Select the same door " +
+                    "to keep your choice or click the other door to change. ");
+                // doorCover = $(this).find($(".door-cover"));
+                // $(doorCover).addClass('bg-primary-dark').removeClass('bg-primary');
+                stage+=1;
+                break;
+            case 2:
+
+                if (this !== revealedDoor){
+                    $(".door-cover").each(function (i,obj) {
+                        $(obj).fadeOut();
+                    });
+                    if (this === correctDoor){
+                        $(this).find($(".door-content")).addClass('bg-success').removeClass('bg-secondary');
+                        info.html("You won the game!");
+                        won = true;
+                    }
+                    else{
+                        $(this).find(doorContent).addClass('bg-danger').removeClass('bg-secondary');
+                        $(correctDoor).find(doorContent).addClass('bg-success').removeClass('bg-secondary');
+                        info.html("You lost the game!");
+
+                    }
+                    if (this === firstChoiceDoor){
+                        console.log("door selected = same as the first");
+                        keepCounter+=1;
+                        $(gamesPlayedKeepCounterObj).html(keepCounter);
+                        if (won){
+                            wonKeepCounter +=1;
+                            $(gamesWonKeepCounterObj).html(wonKeepCounter);
+                        }
+                        $(winKeepPercObj).html(100*(wonKeepCounter/keepCounter)+" %");
+                    }else{
+                        switchCounter+=1;
+                        $(gamesPlayedSwitchCounterObj).html(switchCounter);
+                        if (won){
+                            wonSwitchCounter +=1;
+                            $(gamesWonSwitchCounterObj).html(wonSwitchCounter);
+                        }
+                        $(winSwitchPercObj).html(100*(wonSwitchCounter/switchCounter)+" %");
+                    }
+                    stage+=1;
+                    repeat.css("visibility","visible");
+                    break;
+                }
+                else{
+                    break;
+                }
+            case 3:
+        }
+    });
+    repeat.click(function () {
+       initializeDoors();
     });
     feather.replace();
 });
 
 function initializeDoors() {
-    var correctDoorIndex = Math.floor(Math.random() * 3);
-
-    console.log(correctDoorIndex);
-
+    stage = 1;
+    won = false;
+    $(repeat).css("visibility","hidden");
     doors = $(".door");
+    doors.each(function (i,obj) {
+        $(obj).find(doorCover).fadeIn();
+        $(obj).find(doorContent).removeClass("bg-danger bg-success").addClass("bg-secondary");
+    });
+    correctDoorIndex = Math.floor(Math.random() * 3);
 
     correctDoor = doors[correctDoorIndex];
     wrongDoors = [];
@@ -31,5 +114,5 @@ function initializeDoors() {
     wrongDoors.forEach(function (d) {
         $(d).find(".door-content").html(icon);
     });
-    console.log(doors[correctDoorIndex]);
+    feather.replace();
 }
